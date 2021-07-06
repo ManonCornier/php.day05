76d6fbbdb3cfc9e237110c2aca9067ece7391a89
MANON CORNIER <manon@MacBook-Air-de-MANON-2.local>
2021-07-06 11:56:35 +0200

    exercice 01

---
+++ ex_01/Animal.php
@@ -0,0 +1,41 @@
+<?php
+class Animal {
+    const MAMMAL = 0;
+    const FISH = 1;
+    const BIRD = 2;
+    private $name;
+    private $legs;
+    private $type;
+
+    public function __construct ($blaze,$nblegs,$species) {
+        $this->name =$blaze;
+        $this->legs =$nblegs;
+        
+        switch($this->type) {
+            case Animal::MAMMAL :
+            $this->type ="mammal";
+            break;
+            
+            case Animal::FISH : 
+            $this->type ="fish";
+            break;
+
+            case Animal::BIRD :
+            $this->type ="bird";
+            break;
+        }
+        echo "My name is $blaze and I am a $this->type !\n ";
+    }
+    public function getName () {
+        return $this->name;
+    }
+    public function getLegs () {
+        return $this->legs;
+    }
+    public function getType () {
+        return $this->type;
+    }
+}
+$isidore = new Animal("Isidore", 4, "mamal");
+echo $isidore->getName() . " has " . $isidore->getLegs() . " legs and is a " .$isidore->getType() . ".\n";
+?>
